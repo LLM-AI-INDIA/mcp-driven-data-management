f8d959b5c42763051c6e93a42b4970e2d17c7e7d
BB <97030480+unknown-entity98@users.noreply.github.com>
2025-09-01 06:40:23 +0530

    logo

--- client1.py
+++ client1.py
@@ -442,7 +442,7 @@ with st.sidebar:
         st.button("Clear/Reset", key="clear_button")
 
     st.markdown('<div class="sidebar-logo-label">Build & Deployed on</div>', unsafe_allow_html=True)
-    logo_base64 = get_image_base64("Picture1.png")
+    logo_base64 = get_image_base64("llm.png")
     st.markdown(
     f"""
     <div class="sidebar-logo-row">
@@ -457,7 +457,7 @@ with st.sidebar:
 
 
 # ========== LOGO/HEADER FOR MAIN AREA ==========
-logo_path = "Picture1.png"
+logo_path = "llm.png"
 logo_base64 = get_image_base64(logo_path) if os.path.exists(logo_path) else ""
 if logo_base64:
     st.markdown(
@@ -484,14 +484,14 @@ st.markdown(
             letter-spacing: -2px;
             color: #222;
         ">
-            MCP-Driven Data Management With Natural Language
+            Implementing the Model Context Protocol for Enterprise Data and Insights
         </span>
         <span style="
             font-size: 1.15rem;
             color: #555;
             margin-top: 0.35rem;
         ">
-            Agentic Approach:  NO SQL, NO ETL, NO DATA WAREHOUSING, NO BI TOOL 
+            MCP Unifies Data. Delivers Insights
         </span>
         <hr style="
         width: 80%;
